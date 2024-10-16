02b9b5fc0ae5131f367953bd76a8a1e0afe65c8e
Andi Firmansyah <andfirsyah58@gmail.com>
2024-10-16 18:24:18 +0700

    buat repo

---
+++ js/index.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E3E62',
+        second: '#FF6500',
+        tertiary: '#006BFF',
+      },
+      fontFamily: {
+        poppins : ['Poppins', 'sans-serif'],
+      }
+    }
+  }
+}
