73fa56e51ccf21638534d95f3f79e02387a1a2e9
Andi Firmansyah <andfirsyah58@gmail.com>
2024-10-18 08:22:11 +0700

    menambakan menu bar responsif

--- js/index.js
+++ js/index.js
@@ -7,8 +7,14 @@ tailwind.config = {
         tertiary: '#006BFF',
       },
       fontFamily: {
-        poppins : ['Poppins', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
       }
     }
   }
 }
+
+const menu = document.getElementById('menu-bar');
+const listMenu = document.getElementById('list-menu');
+menu.addEventListener('click', function() {
+    listMenu.classList.toggle('hidden');
+});
